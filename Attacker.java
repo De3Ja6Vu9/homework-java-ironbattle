ad5a71ea54e5b2d71b6a15b09c4574309673048b
Shanice Bandoo <shanicebandoo@gmail.com>
2024-06-20 12:22:50 -0400

    Final changes

--- Attacker.java
+++ Attacker.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public interface Attacker {
+public interface Attacker {
+
+    // The attack method, as required by the Attacker interface.
+    // Determines whether to perform a heavy or weak attack based on the warrior's current stamina,
+    // then adjusts the target's HP and the warrior's stamina accordingly.
+    void attack(Character character);
 }
--- Warrior.java
+++ Warrior.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 // This Warrior class represents a warrior character in the game.
 // It extends the Character class and implements the Attacker interface.
-public class Warrior extends Character implements Attacker {
+public class Warrior extends Character implements Attacker{
     // Stamina is used to perform attacks.
     private int stamina;
     // Strength determines the power of the warrior's attacks.
@@ -12,7 +12,7 @@ public class Warrior extends Character implements Attacker {
 
     // Constructor for the Warrior class.
     // Initializes a new Warrior with the given name, hp, stamina, and strength.
-    public Warrior(String name, int hp, int stamina, int strength) {
+    public Warrior(String name, int hp1, int i, int hp) {
         super(name, hp); // Call to the superclass constructor (Character)
         this.stamina = stamina; // Initialize the warrior's stamina
         this.strength = strength; // Initialize the warrior's strength
@@ -38,25 +38,30 @@ public class Warrior extends Character implements Attacker {
         this.strength = strength;
     }
 
-    // The attack method, as required by the Attacker interface.
-    // Determines whether to perform a heavy or weak attack based on the warrior's current stamina,
-    // then adjusts the target's HP and the warrior's stamina accordingly.
-    @Override
-    public void attack(Character target) {
-        boolean heavyAttack = rand.nextBoolean(); // Decide randomly whether to perform a heavy attack
-        if (heavyAttack && stamina >= 5) { // If a heavy attack is chosen and the warrior has enough stamina...
-            target.setHp(target.getHp() - strength); // ...reduce the target's HP by the warrior's strength...
-            stamina -= 5; // ...and decrease the warrior's stamina by 5.
-            System.out.println(getName() + " performs a Heavy attack on " + target.getName() + " for " + strength + " damage.");
-        } else if (stamina > 0) { // If the warrior doesn't have enough stamina for a heavy attack but can perform a weak attack...
-            int weakDamage = strength / 2; // ...calculate the damage for a weak attack...
-            target.setHp(target.getHp() - weakDamage); // ...reduce the target's HP by the weak damage...
-            stamina += 1; // ...and increase the warrior's stamina by 1.
-            System.out.println(getName() + " performs a Weak attack on " + target.getName() + " for " + weakDamage + " damage.");
-        } else { // If the warrior doesn't have enough stamina for any attack...
-            stamina += 2; // ...recover 2 stamina.
-            System.out.println(getName() + " is out of stamina and recovers 2 stamina.");
+    public void attack(Character character) {
+        Random rand = new Random();
+        boolean heavyAttack = rand.nextBoolean();
+
+        if (stamina < 5) {
+            heavyAttack = false;
         }
+
+        int damage;
+        if (heavyAttack) {
+            damage = strength;
+            stamina -= 5;
+        } else {
+            if (stamina < 1) {
+                damage = 0;
+                stamina += 2;
+            } else {
+                damage = strength / 2;
+                stamina += 1;
+            }
+        }
+
+        character.takeDamage(damage);
+        System.out.println(getName() + " attacks " + character.getName() + " for " + damage + " damage. (" + (heavyAttack ? "Heavy" : "Weak") + " attack)");
     }
-}
+    }
 
